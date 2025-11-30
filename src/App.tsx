d95cdf4b396ff1cd94fcf1c11cdb0d94e19490c2
YeBeenChoi <yebin607@naver.com>
2025-11-30 23:30:58 +0900

    [mod] App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import { appRouter } from '@/routes/appRouter';
+import { appRouter } from '@/routes/routes';
 
 export const App = () => {
   return <RouterProvider router={appRouter} />;
