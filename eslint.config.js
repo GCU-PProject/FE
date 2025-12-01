ddfe5c85bc65a88fbf29c772d6b855bf0ef137fe
YeBeenChoi <yebin607@naver.com>
2025-12-01 18:50:28 +0900

    [chore] ESLint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,10 @@ import globals from 'globals';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default tseslint.config(
   js.configs.recommended,
@@ -12,7 +16,7 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         project: ['./tsconfig.json'],
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
       globals: {
         ...globals.browser,
