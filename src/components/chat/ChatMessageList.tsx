8d28c778f329a0a8976598fc8dd3c242300678d7
Kangho <2351252@naver.com>
2025-12-10 17:52:25 +0900

    [mod]최종 수정

--- src/components/chat/ChatMessageList.tsx
+++ src/components/chat/ChatMessageList.tsx
@@ -106,8 +106,8 @@ export const ChatMessageList = ({
           <Card className='p-4'>
             <div className='flex gap-2'>
               <div className='w-2 h-2 bg-text-tertiary rounded-full animate-bounce' />
-              <div className='w-2 h-2 bg-text-tertiary rounded-full animate-bounce delay-100' />
-              <div className='w-2 h-2 bg-text-tertiary rounded-full animate-bounce delay-200' />
+              <div className='w-2 h-2 bg-text-tertiary rounded-full animate-bounce' style={{ animationDelay: '100ms' }} />
+              <div className='w-2 h-2 bg-text-tertiary rounded-full animate-bounce' style={{ animationDelay: '200ms' }} />
             </div>
           </Card>
         </output>
